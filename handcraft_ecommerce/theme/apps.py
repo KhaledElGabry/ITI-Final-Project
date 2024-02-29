5b2fe526055b45412b48d83d3ea15425e8a37fda
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-02-29 16:26:29 +0200

    Removing suit from theme

--- handcraft_ecommerce/theme/apps.py
+++ handcraft_ecommerce/theme/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
-from suit.apps import DjangoSuitConfig
 
-# class SuitConfig(DjangoSuitConfig):
-#     layout = 'horizontal'
-    # layout = 'vertical'
 
 class ThemeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
