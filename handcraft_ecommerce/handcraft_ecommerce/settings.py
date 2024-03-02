4bef505a1f08273c7e67aeedcb78d51ad1247815
Shorouk Wagih <shoroukwagih1511@gmail.com>
2024-03-02 19:07:56 +0200

    fix mail bug

--- handcraft_ecommerce/handcraft_ecommerce/settings.py
+++ handcraft_ecommerce/handcraft_ecommerce/settings.py
@@ -1,4 +1,12 @@
 from pathlib import Path
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'itifellas@gmail.com'
+EMAIL_HOST_PASSWORD = 'tsgzwmjkboqdxhla'
+DEFAULT_FROM_EMAIL = 'shoroukwagih1511@gmail.com'
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
