2c60bfbe9949fb2e93c8319ce2980a3605d894ac
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-03-03 20:21:09 +0200

    configure project urls for static and media url/root

--- handcraft_ecommerce/handcraft_ecommerce/urls.py
+++ handcraft_ecommerce/handcraft_ecommerce/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +9,5 @@ urlpatterns = [
     path('api/', include('product.urls')),
 
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
