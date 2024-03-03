ec2c3ecab060dbb6d29e2c13a9b2208cb17ffaf7
AbdelrahmanAbdelgawad97 <abdelrahman.abdelgawad571@gmail.com>
2024-03-03 21:19:46 +0200

    modification for admin panal

--- handcraft_ecommerce/handcraft_ecommerce/settings.py
+++ handcraft_ecommerce/handcraft_ecommerce/settings.py
@@ -19,8 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_interface',
-    'colorfield',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
