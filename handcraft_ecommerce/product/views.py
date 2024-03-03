7132a35d14aa677d3f0d6b3121e0b52454a2c2bd
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-03-03 20:10:13 +0200

    Create Product list and details API's

--- handcraft_ecommerce/product/views.py
+++ handcraft_ecommerce/product/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from .models import Product, Category, SubCategory
+from .serializers import ProductSerializer, CategorySerializer, SubCategorySerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics
 
-# Create your views here.
+
+
+
+@api_view(['GET'])
+def productListApi(request):
+     Products = Product.objects.all()
+     data = ProductSerializer(Products, many=True).data
+     return Response({'data':data})
+
+@api_view(['GET'])
+def productDetailsApi(request, id):
+     productDetails = Product.objects.get(id=id)
+     data = ProductSerializer(productDetails).data
+     return Response({'data':data})
+
+
+
