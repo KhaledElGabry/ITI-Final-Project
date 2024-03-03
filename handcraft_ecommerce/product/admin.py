492a56702470fa4b7a3906905adbf2b69927d907
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-03-03 20:31:04 +0200

    Adding models to admin

--- handcraft_ecommerce/product/admin.py
+++ handcraft_ecommerce/product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, ProductImage, Category, SubCategory
 
-# Register your models here.
+
+
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(Category)
+admin.site.register(SubCategory)
