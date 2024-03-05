32866d8cace1af8e01cd7572b41462b5c01aee2f
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-03-05 15:59:29 +0200

    Create more Api's for Product

--- handcraft_ecommerce/product/views.py
+++ handcraft_ecommerce/product/views.py
@@ -3,8 +3,10 @@
 from .models import Product, Category, SubCategory
 from .serializers import ProductSerializer, CategorySerializer, SubCategorySerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.shortcuts import get_object_or_404
 
 
 
@@ -23,6 +25,45 @@ def productDetailsApi(request, id):
      data = ProductSerializer(productDetails).data
      return Response({'data':data})
 
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated, IsAdminUser])
+def addProductApi(request):
+     if request.user.usertype != 'vendor':
+        return Response({'error': 'Only vendors are allowed to add products'}, status=403)
+     
+     productData = request.data
+     newProduct = Product.objects.create(vendor=request.user, **productData)
+     newProduct.save()
+     return Response({'message': 'Product added successfully'})
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated, IsAdminUser])
+def updateProductApi(request, id):
+     if request.user.usertype != 'vendor':
+      return Response({'error': 'Only vendors are allowed to update products'}, status=403)
+     
+     theProductInstance = get_object_or_404(Product, id=id)
+
+     productData = request.data
+     serializer = ProductSerializer(theProductInstance, data=productData, partial=True)
+     if serializer.is_valid():
+         serializer.save()
+         return Response({'message': 'Product updated successfully'})
+     else:
+         return Response(serializer.errors, status=400)
+     return Response({'message': 'Product updated successfully'})
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated, IsAdminUser])
+def removeProductApi(request, id):
+     if request.user.usertype != 'vendor':
+        return Response({'error': 'Only vendors are allowed to remove products'}, status=403)
+    
+
+     theProductInstance = get_object_or_404(Product, id=id)
+     theProductInstance.delete()
+     return Response({'message': 'Product removed successfully'})
 
 
 
