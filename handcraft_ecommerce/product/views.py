b8407f539072f069d9d9f156f6e2f9cc59354bcf
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-03-06 03:34:59 +0200

    Create all Api's for vendor and urls

--- handcraft_ecommerce/product/views.py
+++ handcraft_ecommerce/product/views.py
@@ -1,12 +1,14 @@
-# from django.shortcuts import render
 
 from .models import Product, Category, SubCategory
+from account.models import User
 from .serializers import ProductSerializer, CategorySerializer, SubCategorySerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import permissions
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 
 
@@ -25,45 +27,55 @@ def productDetailsApi(request, id):
      data = ProductSerializer(productDetails).data
      return Response({'data':data})
 
+# Vendor API's
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def productVendorApi(request, id):
+    # GET: List all products belonging to the specified vendor
+    vendor = get_object_or_404(User, id=id, usertype='vendor')
+    vendorProducts = Product.objects.filter(prodVendor=vendor)
+    serializer = ProductSerializer(vendorProducts, many=True)
+    return Response(serializer.data)
+
+
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated, IsAdminUser])
-def addProductApi(request):
-     if request.user.usertype != 'vendor':
-        return Response({'error': 'Only vendors are allowed to add products'}, status=403)
-     
-     productData = request.data
-     newProduct = Product.objects.create(vendor=request.user, **productData)
-     newProduct.save()
-     return Response({'message': 'Product added successfully'})
+@permission_classes([permissions.IsAuthenticated])
+def productCreateVendorApi(request):
+    # POST: Create a new product for the authenticated vendor
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(prodVendor=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['PUT'])
-@permission_classes([IsAuthenticated, IsAdminUser])
-def updateProductApi(request, id):
-     if request.user.usertype != 'vendor':
-      return Response({'error': 'Only vendors are allowed to update products'}, status=403)
-     
-     theProductInstance = get_object_or_404(Product, id=id)
 
-     productData = request.data
-     serializer = ProductSerializer(theProductInstance, data=productData, partial=True)
-     if serializer.is_valid():
-         serializer.save()
-         return Response({'message': 'Product updated successfully'})
-     else:
-         return Response(serializer.errors, status=400)
-     return Response({'message': 'Product updated successfully'})
 
-@api_view(['DELETE'])
-@permission_classes([IsAuthenticated, IsAdminUser])
-def removeProductApi(request, id):
-     if request.user.usertype != 'vendor':
-        return Response({'error': 'Only vendors are allowed to remove products'}, status=403)
-    
+@api_view(['PUT', 'DELETE'])
+@permission_classes([permissions.IsAuthenticated])
+def productUpdateDeleteApi(request, id):
+    try:
+        isProdExist = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
 
-     theProductInstance = get_object_or_404(Product, id=id)
-     theProductInstance.delete()
-     return Response({'message': 'Product removed successfully'})
+    # Check if the authenticated user is the owner of the product
+    if request.user != isProdExist.prodVendor:
+        return Response({'error': 'You do not have permission to update/delete this product'}, status=status.HTTP_403_FORBIDDEN)
+
+    if request.method == 'PUT':
+        # PUT: Update the existing product belonging to the authenticated vendor
+        serializer = ProductSerializer(isProdExist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        # DELETE: Delete the existing product belonging to the authenticated vendor
+        isProdExist.delete()
+        return Response({'message': 'Product deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
 
 
 
@@ -105,7 +117,3 @@ def subCategoryDetailsApi(request, id):
      subCategoriesDetails = SubCategory.objects.get(id=id)
      data = SubCategorySerializer(subCategoriesDetails).data
      return Response({'data':data})
-
-
-
-
