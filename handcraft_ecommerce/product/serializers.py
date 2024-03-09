aed1abadd58d9ea10e7a501dd1f0e97cafd7744a
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-03-09 14:37:45 +0200

    create product functions (Create, Update, Delete)

--- handcraft_ecommerce/product/serializers.py
+++ handcraft_ecommerce/product/serializers.py
@@ -6,14 +6,14 @@ from account.serializers import UserSerializer
 class CategorySerializer(serializers.ModelSerializer):
      class Meta:
         model = Category
-        fields = ['id']
+        fields = '__all__'
 
 
 
 class SubCategorySerializer(serializers.ModelSerializer):
      class Meta:
         model = SubCategory
-        fields = ['id']
+        fields = '__all__'
 
 
 
@@ -23,14 +23,123 @@ class ProductImageSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+      #   def create(self, validated_data):
+     
+      #    productImages=ProductImage()
+      #    productImages.prodImageProduct=validated_data['prodImageProduct']
+      #    productImages.prodImage=validated_data['prodImage']
+        
+
+
+      #    product = Product.objects.create(**validated_data)
+      #    product.save()
+      #    print(product.prodSubCategory.id)
+      #    return product    
+
+
+      
+
+
 
 
 class ProductSerializer(serializers.ModelSerializer):
-     class Meta:
-          model = Product
-          fields = '__all__'
+      class Meta:
+         model = Product
+         fields = '__all__'
 
-     prodVendor = UserSerializer()
-     prodCategory = CategorySerializer()
-     prodSubCategory = SubCategorySerializer()
-     prodImages = ProductImageSerializer(many=True)
+         # prodVendor = UserSerializer()
+         # prodCategory = CategorySerializer()
+         # prodSubCategory = SubCategorySerializer()
+         # prodImages = ProductImageSerializer(many=True)
+
+      def create(self, validated_data):
+     
+         product=Product()
+         product.prodVendor=validated_data['prodVendor']
+         product.prodName=validated_data['prodName']
+         product.prodPrice=validated_data['prodPrice']
+         product.prodDescription=validated_data['prodDescription']
+         # product.prodCategory=validated_data['prodCategory']
+         # prod_sub_category_id = validated_data['prodSubCategory']
+         # print(prod_sub_category_id)
+         product.prodSubCategory=validated_data['prodSubCategory']
+         product.prodOnSale=validated_data['prodOnSale']
+         product.prodImageThumbnail=validated_data['prodImageThumbnail']
+         # product.prodImages=validated_data['prodImages']
+
+
+         product = Product.objects.create(**validated_data)
+         product.save()
+         print(product.prodSubCategory.id)
+         return product    
+      
+
+      def update(self, instance, validated_data):
+        instance.prodVendor = validated_data.get('prodVendor', instance.prodVendor)
+        instance.prodName = validated_data.get('prodName', instance.prodName)
+        instance.prodPrice = validated_data.get('prodPrice', instance.prodPrice)
+        instance.prodDescription = validated_data.get('prodDescription', instance.prodDescription)
+        instance.prodSubCategory = validated_data.get('prodSubCategory', instance.prodSubCategory)
+        instance.prodOnSale = validated_data.get('prodOnSale', instance.prodOnSale)
+        instance.prodImageThumbnail = validated_data.get('prodImageThumbnail', instance.prodImageThumbnail)
+
+
+
+        instance.save()
+        return instance
+      
+      def delete(self, instance):
+        instance.delete()
+
+
+
+
+   
+
+
+
+
+      # prod_sub_category_data = validated_data.pop('prodSubCategory_data', None)
+      # if prod_sub_category_data:
+      #       prod_sub_category, _ = SubCategory.objects.get_or_create(**prod_sub_category_data)
+      # elif prod_sub_category_id:
+      #       prod_sub_category = SubCategory.objects.get(id=prod_sub_category_id)
+      # else:
+      #       prod_sub_category = None
+
+      #   # Create the Product instance
+      # product = Product.objects.create(**validated_data)
+
+      #   # Set the prodSubCategory if available
+      # if prod_sub_category:
+      #       product.prodSubCategory = prod_sub_category
+      #       product.save()
+
+      # return product
+
+
+
+
+      
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      # product.prodRating=validated_data.get('rating', 0.0)
+      # prodSlug
