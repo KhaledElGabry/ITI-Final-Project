04675844d4d7ffa9fe1da8800ae3f15ff6b3e127
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-03-11 02:02:16 +0200

    Delete unused urls

--- handcraft_ecommerce/product/urls.py
+++ handcraft_ecommerce/product/urls.py
@@ -17,16 +17,6 @@ urlpatterns = [
 
 
 
-
-     # vendor API's (ClassBasedView)
-
-     path('v2/create/', views.ProductCreate.as_view(), name='product-create'),
-     path('v2/update/<int:pk>', views.ProductUpdate.as_view(), name='product-update'),
-     path('v2/delete/<int:pk>', views.ProductDelete.as_view(), name='product-delete'),
-
-
-
-
      # category API's (FunctionBasedView)
      path('category/', views.categoryListApi, name='categoryListApi'),
      path('category/<int:id>/', views.categoryDetailsApi, name='categoryDetailsApi'),
